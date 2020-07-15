e0dbdb27eedbee9fb880b899be2b24abca387ba1
Kurt Krafft <kkrafft6609@gmail.com>
2020-07-15 14:30:35 -0500

    took our keys

--- src/modules/googleMapsManager.js
+++ src/modules/googleMapsManager.js
@@ -2,10 +2,10 @@ import keys from "../keys"
 
 const url = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
-// 'location=36.1627,-86.7816&radius=1500&keyword=park&key=AIzaSyDYbWxuIdwSGog0qHV2joDY0reDxdPVPaw'
+
 export default {
     getStop(obj){
-        return fetch(`${url}location=${obj.location}&radius=1500&keyword=${obj.keyword}&key=AIzaSyDYbWxuIdwSGog0qHV2joDY0reDxdPVPaw`, {
+        return fetch(`${url}location=${obj.location}&radius=1500&keyword=${obj.keyword}&key=${keys.google_two}`, {
             method: "GET",
             // mode: 'no-cors'
             // headers: {
@@ -14,7 +14,7 @@ export default {
         }).then(r=> console.log(r))
     },
     getStopTwo(obj){
-        return fetch(proxyurl + `${url}location=${obj.location}&radius=1500&keyword=${obj.keyword}&key=AIzaSyDYbWxuIdwSGog0qHV2joDY0reDxdPVPaw`) // https://cors-anywhere.herokuapp.com/https://example.com
+        return fetch(proxyurl + `${url}location=${obj.location}&radius=1500&keyword=${obj.keyword}&key=${keys.google_two}`) // https://cors-anywhere.herokuapp.com/https://example.com
         .then(response => response.json())
         .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
     }
